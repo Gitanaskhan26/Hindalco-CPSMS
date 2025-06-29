18956919f605bbc7a85ea66250cdd72935b0b038
Anas Khan <anaskhan.ssif@gmail.com>
2025-06-29 20:55:17 +0000

    Image is not loading. is shows a broken image logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,21 +1,15 @@
 import * as React from 'react';
-import Image from 'next/image';
 
-// A wrapper is needed for next/image with fill={true}
-// It should have a position attribute like relative, absolute, fixed, or sticky.
-// The parent components already provide dimensions via className.
 export const Logo = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) => {
+}: React.HTMLAttributes<HTMLImageElement>) => {
   return (
-    <div className={`relative ${className || ''}`} {...props}>
-      <Image
-        src="https://storage.googleapis.com/project-spark-3c32e.appspot.com/a/studio-images/z6nLp1hIeI/logo.svg"
-        alt="Hindalco Logo"
-        fill
-        style={{objectFit: 'contain'}}
-      />
-    </div>
+    <img
+      src="https://storage.googleapis.com/project-spark-3c32e.appspot.com/a/studio-images/z6nLp1hIeI/logo.svg"
+      alt="Hindalco Logo"
+      className={className}
+      {...props}
+    />
   );
 };
