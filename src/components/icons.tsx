07ea300f49afb11b5065b4a0332cfcae5377f7e4
Anas Khan <anaskhan.ssif@gmail.com>
2025-06-29 20:47:21 +0000

    Create a Hindalco Industries logo for this app

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,7 +9,7 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <rect width="32" height="32" rx="8" fill="hsl(var(--primary))" />
+    <circle cx="16" cy="16" r="16" fill="hsl(var(--primary))" />
     <path
       d="M10 10V22H12V16H20V22H22V10H20V14H12V10H10Z"
       fill="hsl(var(--primary-foreground))"
